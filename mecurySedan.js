09dc70d81e897f35a8578bd4ee5cfbd462278213
Leonardo Garcia <leogarcia258@gmail.com>
2022-12-11 12:43:35 -0800

    first p

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,56 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require ('./vehicle').Vehicle // "I used to solution code to find this out" 
+class Car extends VehicleModule {
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+    constructor(make, model, year, color, mileage) {
+
+        super(make, model, year, color, mileage);
+
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+}
+loadPassenger(num); {
+    if (this.passenger < this.maximumPassengers) {
+        if ((num + this.passenger) <= this.maxPassengers) {
+            this.passenger = num;
+            return this.passenger;
+  
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+            }
+    }
+
+    
+    start (); {
+        if (this.fuel > 0) {
+            return this.started == true;
+            console.log("engine started !!!");
+
+        } else {
+
+            return this.started == false;
+            console.log("engine cannot start !!!");
+        }
+    }
+}
+    
+scheduleService(mileage); {
+    if (this.mileage > 30000) {
+        this.scheduleService == true; 
+        return this.scheduleService;
+    }
+}
+//this shows how to call from this module...new
+
+let v = new VehicleModule.Vehicle ("Mecury", "Sedan", "1965", "color", "mileage");
+console.log(v.make);
+
+myCar.loadPassenger(5)
+myCar.start()
+myCar.scheduleService()
+
+console.log(myCar)
